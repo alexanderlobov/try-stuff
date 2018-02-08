8f67056bf11b857022ddcdfd375254acb69bc7d8
Alexander Lobov <alexanderlobov@yandex.ru>
2018-02-08 07:35:47 +0000

    try-preload

---
+++ try-preload/random_num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(0));
+    for (int i = 0; i < 5; ++i) {
+        printf("%d\n", rand() % 100);
+    }
+    return 0;
+}
