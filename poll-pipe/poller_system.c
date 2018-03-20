739af02180db7c9685b3867d0324e1a9a8002991
Alexander Lobov <alexanderlobov@yandex.ru>
2018-03-20 18:39:19 +0300

    Added poller_system experiment

--- poll-pipe/poller_system.c
+++ poll-pipe/poller_system.c
@@ -1,6 +1,7 @@
-#include <zmqpp/zmqpp.hpp>
 #include <unistd.h>
 #include <stdio.h>
+#include <poll.h>
+#include <string.h>
 
 enum {
     PIPE_READ = 0,
@@ -11,26 +12,27 @@ void child(int fd)
 {
     const char *s = "hello";
     write(fd, s, strlen(s));
-    // pause();
     close(fd);
+    // pause();
 }
 
 void parent(int fd)
 {
-    zmqpp::poller p;
-
-    p.add(fd);
+    struct pollfd fds[] = {{fd, POLLIN, 0}}; 
+    const size_t fds_size = sizeof(fds)/sizeof(struct pollfd);
+    printf("fds_size=%ld\n", fds_size);
 
     const size_t BUFSIZE = 4096;
     char buf[BUFSIZE];
     long timeout = 1000;
 
-    while (true) {
-        bool result = p.poll(timeout);
-        printf("poll result is %d\n", result);
-        if (p.has_input(fd)) {
-            printf("input on fd %d: ", fd);
-            int n = read(fd, buf, BUFSIZE-1);
+    while (1) {
+        int poll_result = poll(fds, fds_size, timeout);
+        printf("poll_result is %d\n", poll_result);
+
+        if (fds[0].revents & POLLIN) {
+            printf("input of fd %d: ", fds[0].fd);
+            const int n = read(fd, buf, BUFSIZE - 1);
             if (n > 0) {
                 buf[n] = '\0';
                 printf("'%s'\n", buf);
@@ -39,8 +41,8 @@ void parent(int fd)
                 return;
             }
         }
-        if (p.has_error(fd)) {
-            printf("error on fd %d\n", fd);
+        if (fds[0].revents & POLLHUP) {
+            printf("POLLHUP on fd %d\n", fd);
         }
     }
 }
@@ -52,7 +54,7 @@ int main()
     // 1. exit from child. Is there EOF of error condition on the pipe?
     // 2. kill the child. The same questions.
     // 3. try with zmqpp and system pollers.
-    
+
     int fd[2];
     pipe(fd);
 
@@ -67,4 +69,5 @@ int main()
     } else {
         perror("fork");
     }
+    return 0;
 }
