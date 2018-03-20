e828210e57715aab36759e8bc65c5f7e64a5c182
Alexander Lobov <alexanderlobov@yandex.ru>
2018-03-20 16:27:34 +0300

    Poll pipe experiment

---
+++ poll-pipe/poller_zmq.cpp
@@ -0,0 +1,69 @@
+#include <zmqpp/zmqpp.hpp>
+#include <unistd.h>
+#include <stdio.h>
+
+enum {
+    PIPE_READ = 0,
+    PIPE_WRITE = 1,
+};
+
+void child(int fd)
+{
+    const char *s = "hello";
+    write(fd, s, strlen(s));
+    pause();
+}
+
+void parent(int fd)
+{
+    zmqpp::poller p;
+
+    p.add(fd);
+
+    const size_t BUFSIZE = 4096;
+    char buf[BUFSIZE];
+    long timeout = 1000;
+
+    while (true) {
+        bool result = p.poll(timeout);
+        printf("poll result is %d\n", result);
+        if (p.has_input(fd)) {
+            printf("input on fd %d: ", fd);
+            int n = read(fd, buf, BUFSIZE-1);
+            if (n > 0) {
+                buf[n] = '\0';
+                printf("'%s'\n", buf);
+            } else if (0 == n) {
+                puts("EOF");
+                return;
+            }
+        }
+        if (p.has_error(fd)) {
+            printf("error on fd %d", fd);
+        }
+    }
+}
+
+int main()
+{
+    // create a pipe
+    // spawn a child
+    // 1. exit from child. Is there EOF of error condition on the pipe?
+    // 2. kill the child. The same questions.
+    // 3. try with zmqpp and system pollers.
+    
+    int fd[2];
+    pipe(fd);
+
+    pid_t child_pid = fork();
+
+    if (child_pid > 0) {
+        close(fd[PIPE_WRITE]);
+        parent(fd[PIPE_READ]);
+    } else if (child_pid == 0) {
+        close(fd[PIPE_READ]);
+        child(fd[PIPE_WRITE]);
+    } else {
+        perror("fork");
+    }
+}
